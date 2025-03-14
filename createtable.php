08d2940fa510b8f070a310d68487a6735337e164
satkrit <satkrit15@gmail.com>
2025-03-14 14:44:44 +0545

    navigation bar

--- createtable.php
+++ createtable.php
@@ -8,7 +8,7 @@
 
     $query = "CREATE TABLE users(
     Id INT AUTO_INCREMENT PRIMARY KEY,
-    Name varchar(100) NOT NULL,,
+    Name varchar(100) NOT NULL,
     Email varchar(100) NOT NULL,
     password varchar(100) NOT NULL)"; 
 if(mysqli_query($conn,$query)){
