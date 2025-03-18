7152c4efef9f86b7390111337351733eb31ae3e1
satkrit <satkrit15@gmail.com>
2025-03-18 18:18:54 +0545

    backend

--- createtable.php
+++ createtable.php
@@ -10,7 +10,8 @@
     Id INT AUTO_INCREMENT PRIMARY KEY,
     Name varchar(100) NOT NULL,
     Email varchar(100) NOT NULL,
-    password varchar(100) NOT NULL)"; 
+    password varchar(100) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP)"; 
 if(mysqli_query($conn,$query)){
         echo "<script>alert('Data successfully enterd');</script>";
                 }
